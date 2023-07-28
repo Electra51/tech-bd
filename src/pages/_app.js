9a07244b7096c2b913f23ba07b4bd2564ae7d430
unknown <safayetnurelectra@gmail.com>
2023-07-28 13:07:27 +0600

    Footer added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
+import Footer from '@/components/ui/Footer'
 import Navbar from '@/components/ui/Navbar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <><Navbar /><Component {...pageProps} /></>
+    <><Navbar /><Component {...pageProps} /><Footer /></>
   )
 }
